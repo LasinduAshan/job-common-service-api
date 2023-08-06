191be914ac1ddd1dcdd5b635d33388608cbda516
Lasindu <lasindu@misynergy.com>
2023-08-06 12:31:10 +0530

    Initial commit

---
+++ src/main/java/com/job/common/JobCommonServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.job.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCommonServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCommonServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/job/common/JobCommonServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCommonServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
